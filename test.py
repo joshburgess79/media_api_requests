c48bad9fe21bf5d0207f76c5006fb6bcfaf950a9
joshburgess79 <joshburgess79@gmail.com>
2019-05-12 22:24:11 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-test = [
-    {
-        'series':
-            {'title': 'Doom Patrol'},
-        'episode':
-            {'title': 'Flex Patrol'},
-        'size': 2300439966.0,
-        'sizeleft': 2217656320.0,
-        'timeleft': '00:54:47'}]
