9ea8ecbba5f740a81598f3c814e1bab21db3d00e
joshburgess79 <joshburgess79@gmail.com>
2019-05-12 22:29:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import sonarrApiInfo
 #sonarr_api_key = YOUR SONARR API KEY GOES HERE
 #sonarr_address = YOUR SONARR SERVER'S local address:8989/api
 
-sonarr_api_key = 
-sonarr_address = 
-
 neweps = sonarrApiInfo.sonarrInfo(sonarr_address, sonarr_api_key)
 
 neweps.upcomingep()
