6b6267af5c7ef268a34a04e0b0ecbaa0c59bbfb5
joshburgess79 <joshburgess79@gmail.com>
2019-05-12 22:24:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sonarrApiInfo
 #sonarr_api_key = YOUR SONARR API KEY GOES HERE
 #sonarr_address = YOUR SONARR SERVER'S local address:8989/api
 
-sonarr_api_key = "b6ce3dd706b94cf9bd35998c0c284e6d"
-sonarr_address = 'http://192.168.1.2:8989/api/'
+sonarr_api_key = 
+sonarr_address = 
 
 neweps = sonarrApiInfo.sonarrInfo(sonarr_address, sonarr_api_key)
 
